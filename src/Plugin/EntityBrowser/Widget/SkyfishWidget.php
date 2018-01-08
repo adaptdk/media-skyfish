f6dd3adc9988bda35745785c69020e18b6b5affc
EdgarasDamkus <edgaras@adapt.dk>
2018-01-08 16:16:44 +0200

    updating

---
+++ src/Plugin/EntityBrowser/Widget/SkyfishWidget.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\media_skyfish\Plugin\EntityBrowser\Widget;
+
+use Drupal\entity_browser\Plugin\EntityBrowser\Widget\Upload;
+
+/**
+ * Plugin implementation of the 'skyfish' widget.
+ *
+ * @EntityBrowserWidget(
+ *   id = "skyfishwidget",
+ *   label = @Translation("Skyfish"),
+ *   description = "Adds Skyfish upload integration.",
+ *   auto_select = FALSE
+ * )
+ */
+
+class SkyfishWidget extends Upload {
+
+}
