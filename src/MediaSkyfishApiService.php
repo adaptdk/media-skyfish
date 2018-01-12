4acb0a933685dc7eb3ae4bba1021e15219963263
EdgarasDamkus <edgaras@adapt.dk>
2018-01-12 10:41:12 +0200

    refactoring for a little bit

--- src/MediaSkyfishApiService.php
+++ src/MediaSkyfishApiService.php
@@ -23,14 +23,19 @@ class MediaSkyfishApiService {
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    * @param \GuzzleHttp\Client $client
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(ConfigFactoryInterface $config_factory, Client $client) {
     $config = $config_factory->get('media_skyfish.adminconfig');
+    $this->skyfish_global_user = $config->get('media_skyfish_global_user');
+    $this->skyfish_global_psw = $config->get('media_skyfish_global_password');
     $this->skyfish_api_key = $config->get('media_skyfish_api_key');
     $this->skyfish_secret_api_key = $config->get('media_skyfish_api_secret');
-    $this->client = new Client();
+    $this->client = $client;
+    //TODO these two goes to construct
     $user = \Drupal::entityTypeManager()->getStorage('user')->load(\Drupal::currentUser()->id());
     $this->skyfish_user_api_key = $user->field_skyfish_api_user->value;
     $this->skyfish_user_secret_api_key = $user->field_skyfish_secret_api_key->value;
+    $this->skyfish_username = $user->field_skyfish_username->value;
+    $this->skyfish_psw = $user->field_skyfish_password->value;
   }
 
   /**
@@ -41,45 +46,82 @@ class MediaSkyfishApiService {
     $api = array();
     $api['skyfish_api_key'] = $this->skyfish_user_api_key;
     $api['skyfish_api_secret'] = $this->skyfish_user_secret_api_key;
+    $api['skyfish_user'] = $this->skyfish_username;
+    $api['skyfish_psw'] = $this->skyfish_psw;
     if (empty($api['skyfish_api_key']) || empty($api['skyfish_api_secret'])) {
       $api['skyfish_api_key'] = $this->skyfish_api_key;
       $api['skyfish_api_secret'] = $this->skyfish_secret_api_key;
+      $api['skyfish_user'] = $this->skyfish_global_user;
+      $api['skyfish_psw'] = $this->skyfish_global_psw;
     }
     return $api;
   }
 
-  public function doRequest($url) {
+  public function doRequest() {
 
-    $api=$this->getApi();
+    $api = $this->getApi();
     if (empty($api['skyfish_api_key']) || empty($api['skyfish_api_secret'])) {
       return FALSE;
     }
+    //TODO replace username and password values from fields && check if getHmac returns proper value
+    $request = $this
+      ->client
+      ->request('POST',
+        self::MEDIA_SKYFISH_API_BASE_URL . '/authenticate/userpasshmac',
+        [
+          'json' =>
+            [
+              'username' => 'karolis.bernotas@adapt.dk',
+              'password' => 'vovwued9',
+              'key' => $api['skyfish_api_key'],
+              'ts' => time(),
+              'hmac' => $this->getHmac(),
+            ]
+        ]);
 
-    $hash = hash_hmac('sha1', $api['skyfish_api_key'] . ':' .time(), $api['skyfish_api_secret']);
+    $response = json_decode($request->getBody()->getContents(), TRUE);
 
-    $response = $this->client->request('POST', 'https://api.colourbox.com/authenticate/userpasshmac', [
-      'json' => ['username' => 'karolis.bernotas@adapt.dk', 'password' => 'vovwued9', 'key' => $api['skyfish_api_key'], 'ts' => time(), 'hmac' => $hash]
-    ]);
+    return $response['token'] ?? FALSE;
 
-    $content = json_decode($response->getBody()->getContents(), TRUE);
+  }
 
-    $authorization = 'CBX-SIMPLE-TOKEN Token=' . $content['token'];
+  public function getHmac() {
+    $api = $this->getApi();
 
-    $request_folders = $this->client->request('GET', 'https://api.colourbox.com/folder', [
-      'headers' => [
-        'Authorization' => $authorization,
-      ]
-    ]);
+    return hash_hmac('sha1', $api['skyfish_api_key'] . ':' .time(), $api['skyfish_api_secret']);
+  }
 
-    $folders = json_decode($request_folders->getBody()->getContents(), TRUE);
+  public function getHeader() {
+    $token = $this->doRequest();
 
-    dpm($folders);
+    if (!$token) {
+      return FALSE;
+    }
 
+    return 'CBX-SIMPLE-TOKEN Token=' . $token;
+  }
 
+  public function getFolders() {
 
-    return true;
+    $header = $this->getHeader();
+    if (!$header) {
+      return FALSE;
+    }
 
-    
+    $request_folders = $this
+      ->client
+      ->request(
+        'GET',
+        self::MEDIA_SKYFISH_API_BASE_URL . '/folders',
+        [
+          'headers' => [
+            'Authorization' => $header,
+          ]
+        ]
+      );
+
+    return json_decode($request_folders->getBody()->getContents(), TRUE);
+
   }
 
 
